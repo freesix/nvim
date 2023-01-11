0c16283efffbdc7d8ec96c410836ae5f4a4c2627
freesix <17781444956@163.com>
2023-01-11 21:02:40 +0800

    支持复制到系统剪切板

--- lua/vscode/vs-keymaps.lua
+++ lua/vscode/vs-keymaps.lua
@@ -15,3 +15,21 @@ keymap("n", "J", "5j", opts)
 keymap("n", "K", "5k", opts)
 keymap("n", "H", "^", opts)
 keymap("n", "L", "$", opts)
+
+keymap("v", "<leader>", ":call VSCodeNotify('whichkey.show')<cr>", opts)
+keymap("v", "J", "5j", opts)
+keymap("v", "K", "5k", opts)
+keymap("v", "H", "^", opts)
+keymap("v", "L", "$", opts)
+
+
+-- vim的复制到系统剪切板
+vim.cmd [[
+let s:clip = '/mnt/c/Windows/System32/clip.exe' 
+if executable(s:clip)
+    augroup WSLYank
+        autocmd!
+        autocmd TextYankPost * if v:event.operator ==# 'y' | call system(s:clip, @0) | endif
+    augroup END
+endif
+]]
